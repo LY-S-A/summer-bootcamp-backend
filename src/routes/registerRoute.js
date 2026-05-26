2f039749a8b6b8cdf5054cb8e1fd2ae1253721f5
lysa.waitlist <Lysauniverse30@gmail.com>
2026-05-27 00:43:16 +0100

    Create registerRoute.js

---
+++ src/routes/registerRoute.js
@@ -0,0 +1,13 @@
+// routes/registerRoute.js
+
+const express = require("express");
+
+const {
+  registerUser,
+} = require("../controllers/registerController");
+
+const router = express.Router();
+
+router.post("/", registerUser);
+
+module.exports = router;
