612fa9df29c91101ebfcdd2ad150512ec12fedcb
lysa.waitlist <Lysauniverse30@gmail.com>
2026-05-27 01:26:18 +0100

    Update registerRoute.js

--- src/routes/registerRoute.js
+++ src/routes/registerRoute.js
@@ -1,13 +1,30 @@
-// routes/registerRoute.js
+// // routes/registerRoute.js
+
+// const express = require("express");
+
+// const {
+//   registerUser,
+// } = require("../controllers/registerController");
+
+// const router = express.Router();
+
+// router.post("/", registerUser);
+
+// module.exports = router;
 
 const express = require("express");
 
 const {
   registerUser,
+  verifyPayment,
 } = require("../controllers/registerController");
 
 const router = express.Router();
 
+// REGISTER USER
 router.post("/", registerUser);
+
+// VERIFY PAYMENT
+router.get("/verify", verifyPayment);
 
 module.exports = router;
